dca4d352a28d0a5b0e7d0341ebb3fd191aade1bf
Bao Long <baolong.phung211@gmail.com>
2023-08-02 21:22:23 +0700

    Update lesson1_extra

--- Lesson1.sql
+++ Lesson1.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS Testing_System_Assignment_1;
-CREATE DATABASE Testing_System_Assignment_1;
+CREATE DATABASE IF NOT EXISTS Testing_System_Assignment_1;
 USE Testing_System_Assignment_1;
 
 DROP TABLE IF EXISTS Department;
--- Lesson1_extra.sql
+++ Lesson1_extra.sql
@@ -4,15 +4,15 @@ USE Fresher;
 
 DROP TABLE IF EXISTS Trainee;
 CREATE TABLE Trainee (
-	TraineeID INT PRIMARY KEY AUTO_INCREMENT,
-    Full_Name VARCHAR(50),
-    Birth_Date DATE,
-    Gender ENUM('male', 'female', 'unknown'),
-    ET_IQ INT CHECK(0 <= ET_IQ <= 20),
-    ET_Gmath INT CHECK(0 <= ET_Gmath <= 20),
-    ET_English INT CHECK(0 <= ET_English <= 50),
-    Training_Class VARCHAR(10),
-    Evaluation_Notes TEXT
+	TraineeID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    Full_Name NVARCHAR(50) NOT NULL,
+    Birth_Date DATE NOT NULL,
+    Gender ENUM('male', 'female', 'unknown') NOT NULL,
+    ET_IQ TINYINT UNSIGNED CHECK(ET_IQ <= 20) NOT NULL,
+    ET_Gmath TINYINT UNSIGNED CHECK(ET_Gmath <= 20) NOT NULL,
+    ET_English TINYINT UNSIGNED CHECK(ET_English <= 50) NOT NULL,
+    Training_Class VARCHAR(10) NOT NULL,
+    Evaluation_Notes TEXT(100) NOT NULL
 );
 
 ALTER TABLE Trainee 
@@ -20,17 +20,17 @@ ADD VTI_Account VARCHAR(50) NOT NULL UNIQUE;
 
 /*
 Exercise 2:
-ID INT PRIMARY KEY AUTO_INCREMENT,
-Name VARCHAR(50),
-Code CHAR(5),
-ModifiedDate DATATIME	
+ID TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+Name VARCHAR(50)NOT NULL,
+Code CHAR(5) NOT NULL,
+ModifiedDate DATATIME DEFAULT NOW()	
 
 Exercise 3:
-ID INT PRIMARY KEY AUTO_INCREMENT,
-Name VARCHAR(50),
-BirthDate DATE,
-Gender TINYINT(1),
-IsDeleteFlag TINYINT(1)
+ID TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+Name VARCHAR(50) NOT NULL,
+BirthDate DATE NOT NULL,
+Gender ENUM('0', '1', '2'), --0: Male, 1: Female, 2: Unknown
+IsDeleteFlag ENUM('0', '1') --0: Active, 1: Delete
 */
 
 
