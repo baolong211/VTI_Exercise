32dd68a87df949a50e15e88c6ec951f92705cdb1
Bao Long <baolong.phung211@gmail.com>
2023-08-07 20:27:17 +0700

    Update lesson1

--- Lesson1_extra.sql
+++ Lesson1_extra.sql
@@ -35,13 +35,13 @@ IsDeleteFlag ENUM('0', '1') DEFAULT 0 --0: Active, 1: Delete
 
 INSERT INTO Fresher_management (Full_Name, Birth_Date, Gender, ET_IQ, ET_Gmath, ET_English, Training_Class, Evaluation_Notes, VTI_Account)
 VALUES 
-('Phung Bao Long', '2003-11-02', 'Male', '18', '19', '47', 'Rocket38', 'Good', 'long.phungbao@vtiacademy.edu.vn'),
-('Phung Bao Long1', '2003-12-04', 'Male', '15', '19', '44', 'Rocket38', 'Good', 'long.phungbao1@vtiacademy.edu.vn'),
-('Phung Bao Long2', '2003-11-18', 'Male', '16', '11', '42', 'Rocket38', 'Good', 'long.phungbao2@vtiacademy.edu.vn'),
-('Phung Bao Long3', '2003-11-19', 'Male', '12', '13', '43', 'Rocket38', 'Good', 'long.phungbao3@vtiacademy.edu.vn'),
-('Phung Bao Long4', '2003-11-25', 'Male', '11', '16', '41', 'Rocket38', 'Good', 'long.phungbao4@vtiacademy.edu.vn'),
-('Phung Bao Long5', '2003-03-10', 'Male', '17', '14', '40', 'Rocket38', 'Good', 'long.phungbao5@vtiacademy.edu.vn'),
-('Phung Bao Long6', '2003-07-02', 'Male', '19', '17', '39', 'Rocket38', 'Good', 'long.phungbao6@vtiacademy.edu.vn'),
-('Phung Bao Long7', '2003-10-10', 'Male', '13', '19', '38', 'Rocket38', 'Good', 'long.phungbao7@vtiacademy.edu.vn'),
-('Phung Bao Long8', '2003-10-31', 'Male', '14', '16', '38', 'Rocket38', 'Good', 'long.phungbao8@vtiacademy.edu.vn'),
-('Phung Bao Long9', '2003-08-12', 'Male', '20', '15', '37', 'Rocket38', 'Good', 'long.phungbao9@vtiacademy.edu.vn');
+	('Phung Bao Long', '2003-11-02', 'Male', '18', '19', '47', 'Rocket38', 'Good', 'long.phungbao@vtiacademy.edu.vn'),
+	('Phung Bao Long1', '2003-12-04', 'Male', '15', '19', '44', 'Rocket38', 'Good', 'long.phungbao1@vtiacademy.edu.vn'),
+	('Phung Bao Long2', '2003-11-18', 'Male', '16', '11', '42', 'Rocket38', 'Good', 'long.phungbao2@vtiacademy.edu.vn'),
+	('Phung Bao Long3', '2003-11-19', 'Male', '12', '13', '43', 'Rocket38', 'Good', 'long.phungbao3@vtiacademy.edu.vn'),
+	('Phung Bao Long4', '2003-11-25', 'Male', '11', '16', '41', 'Rocket38', 'Good', 'long.phungbao4@vtiacademy.edu.vn'),
+	('Phung Bao Long5', '2003-03-10', 'Male', '17', '14', '40', 'Rocket38', 'Good', 'long.phungbao5@vtiacademy.edu.vn'),
+	('Phung Bao Long6', '2003-07-02', 'Male', '19', '17', '39', 'Rocket38', 'Good', 'long.phungbao6@vtiacademy.edu.vn'),
+	('Phung Bao Long7', '2003-10-10', 'Male', '13', '19', '38', 'Rocket38', 'Good', 'long.phungbao7@vtiacademy.edu.vn'),
+	('Phung Bao Long8', '2003-10-31', 'Male', '14', '16', '38', 'Rocket38', 'Good', 'long.phungbao8@vtiacademy.edu.vn'),
+	('Phung Bao Long9', '2003-08-12', 'Male', '20', '15', '37', 'Rocket38', 'Good', 'long.phungbao9@vtiacademy.edu.vn');
